98118b682cb07278dbbb6ab18cd672d62862a055
RWojcikowski <radoslaw.chicqen@gmail.com>
2022-06-22 03:06:04 +0200

    login component ui

---
+++ components/Login.js
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import { ChatIcon } from "@chakra-ui/icons";
+import { Box, Button, Center, Stack, Text } from "@chakra-ui/react";
+
+export default function Login() {
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <Center
+      h="100vh">
+        <Stack
+        align="center"
+        bgColor="#403D3D"
+        p={20}
+        rounded="3xl"
+        spacing={3}
+        boxShadow="dark-lg"
+        >
+          <Box
+           bgColor="#722DA9"
+            w="fit-content"
+            p={5}
+            rounded="2xl"
+            boxShadow="dark-lg"
+          >
+            <ChatIcon w="100px" h="100px" color="#fff" />
+          </Box>
+          <Text fontSize="30px" color="grey">
+            Chat App
+          </Text>
+          <Button boxShadow="md" colorScheme="purple"> Zaloguj przez Google</Button>
+        </Stack>
+
+      </Center>
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import Login from "../components/Login"
+import { ChakraProvider } from "@chakra-ui/react"
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return(
+  <ChakraProvider>
+    <Login />
+</ChakraProvider>
+  )
 }
 
 export default MyApp
